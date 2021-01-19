2fdbc3be031f91102846be8b90ef0321c53ea216
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2021-01-19 19:53:30 +0200

    Add files via upload

---
+++ CountryDAOPGSQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightsProject
+{
+    class CountryDAOPGSQL : ICountryDAO
+    {
+        public void Add(Country t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Country Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Country> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Country t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
